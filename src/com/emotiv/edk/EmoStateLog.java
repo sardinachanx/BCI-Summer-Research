eeea65f3b22caedd01ffaa18555b1b403d50dd05
S. Chan <serena.cx13@gmail.com>
2016-07-10 21:23:50 +0800

    Updated formatting

--- src/com/emotiv/edk/EmoStateLog.java
+++ src/com/emotiv/edk/EmoStateLog.java
@@ -3,35 +3,32 @@ package com.emotiv.edk;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.*;
 
-
 /** Simple example of JNA interface mapping and usage. */
-public class EmoStateLog 
-{      
-    public static void main(String[] args) 
-    {
-    	Pointer eEvent			= Edk.INSTANCE.EE_EmoEngineEventCreate();
-    	Pointer eState			= Edk.INSTANCE.EE_EmoStateCreate();
-    	IntByReference userID 	= null;
-    	short composerPort		= 1726;
-    	int option 				= 1;
-    	int state  				= 0;
-    	
-    	userID = new IntByReference(0);
-    	
-    	switch (option) {
-		case 1:
-		{
+public class EmoStateLog {
+
+	public static void main(String[] args) {
+		Pointer eEvent = Edk.INSTANCE.EE_EmoEngineEventCreate();
+		Pointer eState = Edk.INSTANCE.EE_EmoStateCreate();
+		IntByReference userID = null;
+		short composerPort = 1726;
+		int option = 1;
+		int state = 0;
+
+		userID = new IntByReference(0);
+
+		switch (option) {
+		case 1: {
 			if (Edk.INSTANCE.EE_EngineConnect("Emotiv Systems-5") != EdkErrorCode.EDK_OK.ToInt()) {
 				System.out.println("Emotiv Engine start up failed.");
 				return;
 			}
 			break;
 		}
-		case 2:
-		{
+		case 2: {
 			System.out.println("Target IP of EmoComposer: [127.0.0.1] ");
 
-			if (Edk.INSTANCE.EE_EngineRemoteConnect("127.0.0.1", composerPort, "Emotiv Systems-5") != EdkErrorCode.EDK_OK.ToInt()) {
+			if (Edk.INSTANCE.EE_EngineRemoteConnect("127.0.0.1", composerPort,
+					"Emotiv Systems-5") != EdkErrorCode.EDK_OK.ToInt()) {
 				System.out.println("Cannot connect to EmoComposer on [127.0.0.1]");
 				return;
 			}
@@ -41,10 +38,9 @@ public class EmoStateLog
 		default:
 			System.out.println("Invalid option...");
 			return;
-    	}
-    	
-		while (true) 
-		{
+		}
+
+		while (true) {
 			state = Edk.INSTANCE.EE_EngineGetNextEvent(eEvent);
 
 			// New event needs to be handled
@@ -59,10 +55,10 @@ public class EmoStateLog
 					Edk.INSTANCE.EE_EmoEngineEventGetEmoState(eEvent, eState);
 					float timestamp = EmoState.INSTANCE.ES_GetTimeFromStart(eState);
 					System.out.println(timestamp + " : New EmoState from user " + userID.getValue());
-					
+
 					System.out.print("WirelessSignalStatus: ");
 					System.out.println(EmoState.INSTANCE.ES_GetWirelessSignalStatus(eState));
-					
+
 					if (EmoState.INSTANCE.ES_ExpressivIsBlink(eState) == 1)
 						System.out.println("Blink");
 					if (EmoState.INSTANCE.ES_ExpressivIsLeftWink(eState) == 1)
@@ -73,27 +69,26 @@ public class EmoStateLog
 						System.out.println("LookingLeft");
 					if (EmoState.INSTANCE.ES_ExpressivIsLookingRight(eState) == 1)
 						System.out.println("LookingRight");
-					
+
 					System.out.print("ExcitementShortTerm: ");
 					System.out.println(EmoState.INSTANCE.ES_AffectivGetExcitementShortTermScore(eState));
 					System.out.print("ExcitementLongTerm: ");
 					System.out.println(EmoState.INSTANCE.ES_AffectivGetExcitementLongTermScore(eState));
 					System.out.print("EngagementBoredom: ");
 					System.out.println(EmoState.INSTANCE.ES_AffectivGetEngagementBoredomScore(eState));
-					
+
 					System.out.print("CognitivGetCurrentAction: ");
 					System.out.println(EmoState.INSTANCE.ES_CognitivGetCurrentAction(eState));
 					System.out.print("CurrentActionPower: ");
 					System.out.println(EmoState.INSTANCE.ES_CognitivGetCurrentActionPower(eState));
 				}
-			}
-			else if (state != EdkErrorCode.EDK_NO_EVENT.ToInt()) {
+			} else if (state != EdkErrorCode.EDK_NO_EVENT.ToInt()) {
 				System.out.println("Internal error in Emotiv Engine!");
 				break;
 			}
 		}
-    	
-    	Edk.INSTANCE.EE_EngineDisconnect();
-    	System.out.println("Disconnected!");
-    }
+
+		Edk.INSTANCE.EE_EngineDisconnect();
+		System.out.println("Disconnected!");
+	}
 }
